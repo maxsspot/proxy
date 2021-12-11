c545e60569408101018e9c2ab40e30abaf2a6e0b
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-12-11 08:39:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const element = document.getElementById('find')
 element.addEventListener("click", () => {
        //document.getElementById ("nowebsite").style.display = "none";
        var valueForSite = document.getElementById ("website_url").value;
-       var newSite = document.createElement("iframe");
+       var newSite = document.getElementById ("iframe");
        newSite.setAttribute("src", valueForSite);
-       newSite.style.width = "3500px";
+       newSite.style.width = "500px";
        newSite.style.height = "500px";
        document.body.appendChild(newSite);
 });
